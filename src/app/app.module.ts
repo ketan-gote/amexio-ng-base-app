94a58b1e284b13507d5fdfcfe1707fae179b98ff
ketangote <ketan.gote@metamagic.in>
2018-05-23 10:34:36 +0530

    Amexio NG Base App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AmexioWidgetModule} from 'amexio-ng-extensions';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AmexioWidgetModule
   ],
   providers: [],
   bootstrap: [AppComponent]
