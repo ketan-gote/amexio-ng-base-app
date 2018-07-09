2edc14336e5a31e85efe2390317bf72ea9d9afc1
ketan-gote <ketan-gote>
2018-07-09 05:08:39 +0000

    Initial commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,5 @@
 /**
- * Created by Sagar Jadhav.
+ * Created by sagar on 12/2/18.
  */
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
@@ -12,16 +12,8 @@ import {AmexioWidgetModule} from "amexio-ng-extensions";
 const routes: Routes = [
   { path: '', component:HomeComponent,
     children: [
-          	{
-     	path: 'core/dashboard', loadChildren: './../pages/employeeportal/core/dashboard/dashboard.module#DashboardModule'
-     	},	
-	     	{
-     	path: 'core/employeelist', loadChildren: './../pages/employeeportal/core/employeelist/employeelist.module#EmployeelistModule'
-     	},	
-	     	{
-     	path: 'core/employee', loadChildren: './../pages/employeeportal/core/employee/employee.module#EmployeeModule'
-     	},	
-	    ]
+
+    ]
   }
 ];
 
