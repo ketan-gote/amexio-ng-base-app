ad887978d4c8152cc07fd79a9cfe454e02d5b9b5
ketan-gote <ketan-gote>
2018-07-16 11:00:07 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# July Twelve
+# July Sixteen
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.7.
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,10 +5,9 @@ import {Router} from "@angular/router";
   templateUrl: 'home.html'
 })
 export class HomeComponent {
-   menuData:any;	
   constructor(private _router : Router){
-    
- this.menuData=[{"text":"Core","expand":true,"children":[{"text":"TestUi","routerLink":"core/testui","selected":false},{"text":"NewUI","routerLink":"core/newui","selected":false},{"text":"CountryState","routerLink":"core/countrystate","selected":false}]}];   }
+
+  }
   routeToLink(data: any){
      if(!data.hasOwnProperty('children') && data.routerLink)
       this._router.navigate([data.routerLink]);
